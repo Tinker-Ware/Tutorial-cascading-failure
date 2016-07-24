90b2cc73c0bd1d75cd450a9f819457d51de6eee3
Leonel Perea <iticleonel.len@gmail.com>
2016-07-23 19:36:41 -0500

    add images with require

--- src/containers/MainStore.js
+++ src/containers/MainStore.js
@@ -56,16 +56,16 @@ export class MainStore extends Component {
 				<div className="container">
 					<div className="brand">
 						<div className="col-md-3 brand-grid">
-							<img src="images/ic.png" className="img-responsive" alt=""/>
+							<img src={require("../images/ic.png")} className="img-responsive" alt=""/>
 						</div>
 						<div className="col-md-3 brand-grid">
-							<img src="images/ic1.png" className="img-responsive" alt=""/>
+							<img src={require("../images/ic1.png")} className="img-responsive" alt=""/>
 						</div>
 						<div className="col-md-3 brand-grid">
-							<img src="images/ic2.png" className="img-responsive" alt=""/>
+							<img src={require("../images/ic2.png")} className="img-responsive" alt=""/>
 						</div>
 						<div className="col-md-3 brand-grid">
-							<img src="images/ic3.png" className="img-responsive" alt=""/>
+							<img src={require("../images/ic3.png")} className="img-responsive" alt=""/>
 						</div>
 						<div className="clearfix"></div>
 					</div>
